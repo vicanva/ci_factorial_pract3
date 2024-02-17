6449b4f7aa01513875f70b32e887167f90a7ac23
vicanva <vte.candel@gmail.com>
2024-02-17 14:24:35 +0100

    ok

--- proves/tests/test_factorial.py
+++ proves/tests/test_factorial.py
@@ -1,22 +1,28 @@
 import unittest
+
 from src.factorial import factorials
 
 
-class TestSuma(unittest.TestCase):
+class TestFactorial(unittest.TestCase):
 
     def test_factorial_1(self):
+        """ probem que el factorial de 1 siga 1"""
         self.assertEqual(factorials.factorial(self,1), 1)
 
     def test_error_no_enter(self):
+        """Probem que se pase un string"""
         with self.assertRaises(TypeError):
             factorials.factorial(self,"a")
 
     def test_error_num_negatiu(self):
+        """Probem que se pase un numero negatiu"""
         with self.assertRaises(ValueError):
             factorials.factorial(self,-1)
 
     def test_factorial_5(self):
+        """probem que el factorial de 5 es 120"""
         self.assertEqual(factorials.factorial(self,5), 120)
+
 
 if __name__ == '__main__':
     unittest.main()
--- src/factorial.py
+++ src/factorial.py
@@ -1,17 +1,29 @@
+"""Mòdul per calcular el factorial d'un nombre."""
 
 from math import factorial
 
-
 class factorials:
     def factorial(self, n):
+        """
+        Calcula el factorial d'un nombre.
+
+        Paràmetres:
+            n (int): El nombre del qual es vol calcular el factorial.
+
+        Retorna:
+            int: El factorial de n.
+
+        Excepcions:
+            ValueError: Si n és negatiu.
+            TypeError: Si n és decimal.
+        """
         if n < 0:
             raise ValueError("No poden ser numeros negatius")
 
-        if  isinstance(n, float):
+        if isinstance(n, float):
             raise TypeError("No val posar lletres")
 
         if n == 0:
             return 1
         else:
             return n * factorial(n-1)
-
